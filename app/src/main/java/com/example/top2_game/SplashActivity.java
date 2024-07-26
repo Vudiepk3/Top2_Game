b0ded7276ae0ba63cc8b0d1d099bd84f6c89abee
Vudiepk3 <110141392+Vudiepk3@users.noreply.github.com>
2024-07-26 16:26:15 +0700

    first commit

---
+++ app/src/main/java/com/example/top2_game/SplashActivity.java
@@ -0,0 +1,19 @@
+package com.example.top2_game;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+@SuppressLint("CustomSplashScreen")
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash);
+
+    }
+}
